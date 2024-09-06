ef2aabaced600a02c6a715c99aa8968558bcd137
rodnei-c <rmcaetano04@gmail.com>
2024-09-06 13:20:17 -0300

    prova 2

---
+++ rodneicaetano/src/main/java/com/example/rodneicaetano/RodneicaetanoApplication.java
@@ -0,0 +1,13 @@
+package com.example.rodneicaetano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodneicaetanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodneicaetanoApplication.class, args);
+	}
+
+}
